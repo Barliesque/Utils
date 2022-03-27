6b5aafc90e7c1268fe038e5d8427ff3421519266
David Barlia <barliesque@gmail.com>
2022-03-27 11:48:37 -0700

    Add UpdateManager

---
+++ Scripts/UpdateManager.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public sealed class UpdateManager : MonoBehaviour
+{
+	static UpdateManager _inst;
+
+	event UpdateHandler _frameUpdates;
+	event UpdateHandler _lateUpdates;
+	event UpdateHandler _fixedUpdates;
+	event UpdateHandler _30FPSUpdates;
+	event UpdateHandler _oneSecUpdates;
+	event UpdateHandler _scaledUpdates;
+
+	const int MILLISECS_30FPS = 1000 / 30;
+	const int MILLISECS_1SEC = 1000;
+
+	Stopwatch timer30FPS;
+	Stopwatch timer1Sec;
+	Stopwatch timerFixed;
+
+	public delegate void UpdateHandler(float deltaTime);
+
+	public enum Timing
+	{
+		Frame, Late, Fixed, Frame30FPS, OncePerSecond, Scaled
+	}
+
+	public UpdateManager()
+	{
+		if (_inst != null) throw new Exception("UpdateManager is a singleton class and may not be instantiated more than once.");
+		_inst = this;
+	}
+
+	void Awake()
+	{
+		timer30FPS = new Stopwatch();
+		timer30FPS.Start();
+		timer1Sec = new Stopwatch();
+		timer1Sec.Start();
+		timerFixed = new Stopwatch();
+		timerFixed.Start();
+	}
+
+	public void AddUpdate(UpdateHandler update, Timing timing = Timing.Frame)
+	{
+		RemoveUpdate(update, timing);
+		switch(timing) {
+			case Timing.Fixed:         _fixedUpdates     += update; break;
+			case Timing.Frame:         _frameUpdates     += update; break;
+			case Timing.Late:          _lateUpdates      += update; break;
+			case Timing.Frame30FPS:    _30FPSUpdates     += update; break;
+			case Timing.OncePerSecond: _oneSecUpdates    += update; break;
+			case Timing.Scaled:        _scaledUpdates    += update; break;
+		}
+	}
+
+	public void RemoveUpdate(UpdateHandler update, Timing timing = Timing.Frame)
+	{
+		switch(timing) {
+			case Timing.Fixed:         _fixedUpdates     -= update; break;
+			case Timing.Frame:         _frameUpdates     -= update; break;
+			case Timing.Late:          _lateUpdates      -= update; break;
+			case Timing.Frame30FPS:    _30FPSUpdates     -= update; break;
+			case Timing.OncePerSecond: _oneSecUpdates    -= update; break;
+			case Timing.Scaled:        _scaledUpdates    -= update; break;
+		}
+	}
+
+	public void RemoveAll()
+	{
+		_fixedUpdates = null;
+		_frameUpdates = null;
+		_lateUpdates = null;
+		_30FPSUpdates = null;
+		_oneSecUpdates = null;
+		_scaledUpdates = null;
+	}
+
+	//....................................
+
+	void Update ()
+	{
+		if (_frameUpdates != null)
+			_frameUpdates.Invoke(Time.unscaledDeltaTime);
+
+		if (_scaledUpdates != null)
+			_scaledUpdates.Invoke(Time.deltaTime);
+
+		// Err on the side of more frequent updates to account for framerate fluctuations
+		if (timer30FPS.ElapsedMilliseconds + Time.unscaledDeltaTime >= MILLISECS_30FPS) {
+			if (_30FPSUpdates != null)
+				_30FPSUpdates.Invoke(timer30FPS.ElapsedMilliseconds / 1000f);
+			timer30FPS.Reset();
+			timer30FPS.Start();
+		}
+
+		if (timer1Sec.ElapsedMilliseconds >= MILLISECS_1SEC) {
+			if (_oneSecUpdates != null)
+				_oneSecUpdates.Invoke(timer1Sec.ElapsedMilliseconds / 1000f);
+			timer1Sec.Reset();
+			timer1Sec.Start();
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (_fixedUpdates != null)
+			_fixedUpdates.Invoke(timerFixed.ElapsedMilliseconds / 1000f);
+		timerFixed.Reset();
+		timerFixed.Start();
+	}
+
+	void LateUpdate()
+	{
+		if (_lateUpdates != null)
+			_lateUpdates.Invoke(Time.unscaledDeltaTime);
+	}
+
+	//.....................................................
+
+	static public void Add(UpdateHandler update, Timing timing = Timing.Frame)
+	{
+		if (_inst == null) {
+			var go = new GameObject("UpdateManager");
+			_inst = go.AddComponent<UpdateManager>();
+		}
+
+		_inst.AddUpdate(update, timing);
+	}
+
+	static public void Remove(UpdateHandler update, Timing timing = Timing.Frame)
+	{
+		if (_inst != null)
+			_inst.RemoveUpdate(update, timing);
+	}
+
+}
