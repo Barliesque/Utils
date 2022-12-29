c23b3b027d9c3f01d1575a6eb08ecccd92a3be1b
David Barlia <barliesque@gmail.com>
2022-12-28 20:18:46 -0800

    Added variations of MonoBehaviour.Play() allowing parameters to be passed

--- Scripts/Extensions/MonoBehaviourExtensions.cs
+++ Scripts/Extensions/MonoBehaviourExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
@@ -8,19 +7,58 @@ namespace Barliesque.Utils
 
 	static public class MonoBehaviourExtensions
 	{
+		public delegate void Callback(float t);
 		
-		static public Coroutine Play(this MonoBehaviour owner, float duration, Action<float> callback, bool scaledTime = false)
+		static public Coroutine Play(this MonoBehaviour owner, float duration, Callback callback, bool scaledTime = false)
 		{
 			return owner.StartCoroutine(PlayFunction(duration, callback, scaledTime));
 		}
 
-		static private IEnumerator PlayFunction(float duration, Action<float> callback, bool scaledTime)
+		static private IEnumerator PlayFunction(float duration, Callback callback, bool scaledTime)
 		{
 			var timer = 0f;
 			do
 			{
 				timer += scaledTime ? Time.deltaTime : Time.unscaledDeltaTime;
 				callback(Mathf.Clamp01(timer / duration));
+				yield return null;
+			} while (timer < duration);
+		}
+		
+
+		public delegate void Callback<T>(float t, T value);
+
+		static public Coroutine Play<T>(this MonoBehaviour owner, float duration, Callback<T> callback, T value, bool scaledTime = false)
+		{
+			return owner.StartCoroutine(PlayFunction(duration, callback, value, scaledTime));
+		}
+
+		static private IEnumerator PlayFunction<T>(float duration, Callback<T> callback, T value, bool scaledTime)
+		{
+			var timer = 0f;
+			do
+			{
+				timer += scaledTime ? Time.deltaTime : Time.unscaledDeltaTime;
+				callback(Mathf.Clamp01(timer / duration), value);
+				yield return null;
+			} while (timer < duration);
+		}
+		
+		
+		public delegate void Callback<in T, in U>(float t, T value1, U value2);
+		
+		static public Coroutine Play<T,U>(this MonoBehaviour owner, float duration, Callback<T,U> callback, T value1, U value2, bool scaledTime = false)
+		{
+			return owner.StartCoroutine(PlayFunction(duration, callback, value1, value2, scaledTime));
+		}
+
+		static private IEnumerator PlayFunction<T,U>(float duration, Callback<T,U> callback, T value1, U value2, bool scaledTime)
+		{
+			var timer = 0f;
+			do
+			{
+				timer += scaledTime ? Time.deltaTime : Time.unscaledDeltaTime;
+				callback(Mathf.Clamp01(timer / duration), value1, value2);
 				yield return null;
 			} while (timer < duration);
 		}
