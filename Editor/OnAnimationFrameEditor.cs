3984a9699bdeeba9314df3a84aaaa596c394191c
David Barlia <barliesque@gmail.com>
2024-10-10 19:53:22 -0700

    Added:  OnAnimationFrame
    Sends a message when playhead reaches specified frame

---
+++ Editor/OnAnimationFrameEditor.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEditor;
+using Barliesque.InspectorTools.Editor;
+
+namespace Shattered.Editor
+{
+	
+	[CustomEditor(typeof(OnAnimationFrame))]
+	public class OnAnimationFrameEditor : EditorBase<OnAnimationFrame>
+	{
+		override protected bool ShowScriptField => false;
+
+		override protected void CustomInspector(OnAnimationFrame inst)
+		{
+			var frame = PropertyField("_frame").intValue;
+			var fps = PropertyField("_fps", "FPS").intValue;
+			var seconds = frame / fps;
+			var frames = frame - (fps * seconds);
+			EditorGUILayout.LabelField("Time", $"{seconds}:{frames:00}");
+
+			EditorGUILayout.Space();
+			PropertyField("_message");
+
+			EditorGUI.indentLevel++;
+			var paramType = (OnAnimationFrame.ParamType)PropertyField("_parameterType").intValue;
+			switch (paramType)
+			{
+				case OnAnimationFrame.ParamType.None:
+					break;
+				case OnAnimationFrame.ParamType.Int:
+					PropertyField("_intParam", "Int Value");
+					break;
+				case OnAnimationFrame.ParamType.Float:
+					PropertyField("_floatParam", "Float Value");
+					break;
+				case OnAnimationFrame.ParamType.String:
+					PropertyField("_stringParam", "String Value");
+					break;
+				case OnAnimationFrame.ParamType.Animator:
+					EditorTools.HelpBox("A reference to this Animator will be passed.", MessageType.Info);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+			EditorGUI.indentLevel--;
+			
+			EditorGUILayout.Space();
+			PropertyField("_sendOnEachLoop");
+			PropertyField("_sendUpwards");
+			PropertyField("_requireReceiver");
+		}
+	}
+}
