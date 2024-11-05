41edfb863ce4b424412407d1432eaf668cd5b4fe
David Barlia <barliesque@gmail.com>
2024-11-04 18:21:01 -0800

    Added:  LinearLimits.Center

--- Scripts/LinearLimits.cs
+++ Scripts/LinearLimits.cs
@@ -26,6 +26,7 @@ namespace Barliesque.Utils
 		public float Lerp(float t) => Mathf.Lerp(Start, End, t);
 		public float LerpUnclamped(float t) => Mathf.LerpUnclamped(Start, End, t);
 		public float InverseLerp(float value) => Mathf.InverseLerp(Start, End, value);
+		public float Center => (Start + End) * 0.5f;
 		
 		/// <summary>
 		/// Get a random value within the specified limits.
