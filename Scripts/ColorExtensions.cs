d18ccc5abe0a37485f3239cb7223e710fd5f28b5
David Barlia <barliesque@gmail.com>
2024-11-16 00:21:54 -0800

    Added Gradient Lerp

--- Scripts/ColorExtensions.cs
+++ Scripts/ColorExtensions.cs
@@ -44,6 +44,68 @@ namespace Barliesque.Utils
 		{
 			return new Color(Mathf.Clamp01(color.r), Mathf.Clamp01(color.g), Mathf.Clamp01(color.b), Mathf.Clamp01(color.a));
 		}
+
+		static public Gradient Lerp(this Gradient target, Gradient a, Gradient b, float t)
+		{
+			if (t <= 0f) return target.CopyFrom(a);
+			if (t >= 1f) return target.CopyFrom(b);
+
+			var countA = a.alphaKeys.Length;
+			var countB = b.alphaKeys.Length;
+			var alphaCount = countA + countB;
+			var alphaKeys = (target.alphaKeys.Length != alphaCount) ? new GradientAlphaKey[alphaCount] : target.alphaKeys;
+			for (int i = 0; i < alphaCount; i++)
+			{
+				if (i < countA)
+				{
+					var aKey = a.alphaKeys[i];
+					var time = aKey.time;
+					var alpha = Mathf.Lerp(aKey.alpha, b.Evaluate(time).a, t);
+					alphaKeys[i] = new GradientAlphaKey(alpha, time);
+				}
+				else
+				{
+					var aKey = b.alphaKeys[i - countA];
+					var time = aKey.time;
+					var alpha = Mathf.Lerp(a.Evaluate(time).a, aKey.alpha, t);
+					alphaKeys[i] = new GradientAlphaKey(alpha, time);
+				}
+			}
+
+			countA = a.colorKeys.Length;
+			countB = b.colorKeys.Length;
+			var colorCount = countA + countB;
+			var colorKeys = (target.colorKeys.Length != colorCount) ? new GradientColorKey[colorCount] : target.colorKeys;
+			for (int i = 0; i < colorCount; i++)
+			{
+				if (i < countA)
+				{
+					var cKey = a.colorKeys[i];
+					var time = cKey.time;
+					var color = Color.Lerp(cKey.color, b.Evaluate(time), t);
+					colorKeys[i] = new GradientColorKey(color, time);
+				}
+				else
+				{
+					var cKey = b.colorKeys[i - countA];
+					var time = cKey.time;
+					var color = Color.Lerp(a.Evaluate(time), cKey.color, t);
+					colorKeys[i] = new GradientColorKey(color, time);
+				}
+			}
+			target.SetKeys(colorKeys, alphaKeys);
+			return target;
+		}
+		
+
+		static public Gradient CopyFrom(this Gradient to, Gradient from)
+		{
+			// to.SetKeys(from.colorKeys, from.alphaKeys);
+			to.alphaKeys = from.alphaKeys;
+			to.colorKeys = from.colorKeys;
+			to.colorSpace = from.colorSpace;
+			return to;
+		}
 		
 	}
 	
