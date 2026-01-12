217e0cfeb6ae27431a84a9f62ca9cdebc255da95
David Barlia <barliesque@gmail.com>
2026-01-12 09:50:27 -0800

    LinearLimits:  Added SoftClamp() and operator support

--- Scripts/LinearLimits.cs
+++ Scripts/LinearLimits.cs
@@ -29,6 +29,10 @@ namespace Barliesque.Utils
 		public float InverseLerpUnclamped(float value) => (value - Start) / (End - Start);
 		public float Center => (Start + End) * 0.5f;
 		
+		/// <summary> Smoothly clamps within the range, avoiding hard stops at the range limits. </summary>
+		/// <param name="value"> The original value to be clamped. </param>
+		public float SoftClamp(float value) => Mathf.SmoothStep(Start, End, (value - Start) / (End - Start));
+
 		/// <summary>
 		/// Get a random value within the specified limits.
 		/// </summary>
@@ -49,6 +53,26 @@ namespace Barliesque.Utils
 		override public int GetHashCode()
 		{
 			return HashCode.Combine(Start, End);
+		}
+
+		static public LinearLimits operator *(LinearLimits limits, float multiplier)
+		{
+			return new LinearLimits(limits.Start * multiplier, limits.End * multiplier);
+		}
+
+		static public LinearLimits operator /(LinearLimits limits, float divisor)
+		{
+			return new LinearLimits(limits.Start / divisor, limits.End / divisor);
+		}
+
+		static public LinearLimits operator +(LinearLimits limits, float offset)
+		{
+			return new LinearLimits(limits.Start + offset, limits.End + offset);
+		}
+
+		static public LinearLimits operator -(LinearLimits limits, float offset)
+		{
+			return new LinearLimits(limits.Start - offset, limits.End - offset);
 		}
 	}
 }
